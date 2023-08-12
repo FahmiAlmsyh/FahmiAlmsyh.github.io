34cf6e3942e2460022c9af3355ab2ab728274dab
FahmiAlmsyh <alamsyahf100@gmail.com>
2023-08-12 10:35:26 +0700

    remove

--- main.js
+++ main.js
@@ -38,16 +38,6 @@ const handleFirstTab = (e) => {
     }
   });
 
-  // Navbar Toggle Button
-const toggleButton = document.getElementsByClassName("toggle")[0];
-const navbarItem = document.getElementsByClassName("navbar-item");
-toggleButton.addEventListener("click", function () {
-  // transition navbar item
-  for (let i = 0; i < navbarItem.length; i++) {
-    navbarItem[i].classList.toggle("active");
-  }
-});
-
   
 //   let header = document.querySelector("header");
 //   let menu = document.querySelector("#menu-icon");
